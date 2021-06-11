56438b0984f87cf252c221f6d14a79669ef7bcac
chakmol <chakmol@localhost.localdomain>
2021-06-10 20:50:44 -0500

    Create Controllers

---
+++ src/main/java/com/example/Springlineamientos/Controllers/UserController.java
@@ -0,0 +1,17 @@
+package com.example.Springlineamientos.Controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class UserController {
+    @GetMapping("/")
+    public String index(){
+        return "index";
+    }
+
+    @GetMapping("/userform")
+    public String userForm(){
+        return "user-form/user-view";
+    }
+}
